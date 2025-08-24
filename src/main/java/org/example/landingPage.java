8f481411dbe1a0e1c041251127a75347ef96794d
Ken <kennoschese@gmail.com>
2025-08-24 15:06:13 +0800

    Completed 80% of the landing page.

--- src/main/java/org/example/landingPage.java
+++ src/main/java/org/example/landingPage.java
@@ -2,20 +2,18 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 
-//green#84b477, red#d85259, #e8ced6, blue#b7caef, yellow#ffe786
 
 public class landingPage extends JFrame {
     public landingPage() throws IOException, FontFormatException {
+        setTitle("Para! Jeepney Route System");
         setSize(1920, 1080);
-        setTitle("Para!");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); // Center the window
         setLayout(new BorderLayout());
-
-        JPanel mainPage = new JPanel();
-        mainPage.setPreferredSize(new Dimension(1644, 960));
-        mainPage.setLayout(new BorderLayout());
 
         JPanel head = new JPanel();
         head.setPreferredSize(new Dimension(1181, 160));
@@ -31,88 +29,186 @@ public class landingPage extends JFrame {
         JPanel center = new JPanel();
 
         JPanel contentPane = new JPanel();
-        contentPane.setPreferredSize(new Dimension(1000, 800));
+        contentPane.setPreferredSize(new Dimension(1200, 800));
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 
-        roundPanel textContainer = new roundPanel(30);
-        textContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 150, 50));
-        textContainer.setBackground(new Color(0x84b477));
-        textContainer.setAlignmentX(Component.CENTER_ALIGNMENT);
+        roundPanel infoContainer = new roundPanel(30);
+        infoContainer.setPreferredSize(new Dimension(1200, 300));
+        infoContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 100, -75));
+        infoContainer.setBackground(new Color(0xffe786));
 
-        JPanel wcContainer = new JPanel();
-        wcContainer.setLayout(new BoxLayout(wcContainer, BoxLayout.Y_AXIS));
-        wcContainer.setAlignmentX(Component.CENTER_ALIGNMENT);
-        wcContainer.setBackground(new Color(0x84b477));
+        JPanel infoText = new JPanel();
+        infoText.setBackground(new Color(0xffe786));
+        infoText.setLayout(new BoxLayout(infoText, BoxLayout.Y_AXIS));
 
-        JLabel wcText = new JLabel("Welcome Ken!");
-        wcText.setFont(loadCustomFont("ProjectFiles/DMSansItalic.ttf", 20f));
-        wcText.setForeground(new Color(0xffe786));
-        JLabel wcQuestion = new JLabel();
-        wcQuestion.setText("<html>Where do you want<br>to go?</html>");
-        wcQuestion.setFont(loadCustomFont("ProjectFiles/DMSansBold.ttf", 40f));
+        JLabel wcText = new JLabel("Welcome to Para!");
+        wcText.setFont(loadCustomFont("ProjectFiles/DMSansItalic.ttf", 16f));
+        wcText.setForeground(new Color(0x84b477));
 
-        wcContainer.add(wcText);
-        wcContainer.add(Box.createVerticalStrut(20));
-        wcContainer.add(wcQuestion);
+        JLabel heading = new JLabel("<html>Your smart companion for navigating Davao<br>City's Jeepney routes.</html>");
+        heading.setFont(loadCustomFont("ProjectFiles/DMSansBold.ttf", 22f));
 
-        JPanel inputContainer = new JPanel();
-        inputContainer.setLayout(new BoxLayout(inputContainer, BoxLayout.Y_AXIS));
-        inputContainer.setAlignmentX(Component.CENTER_ALIGNMENT);
-        inputContainer.setBackground(new Color(0x84b477));
+        JLabel bullet = new JLabel();
+        bullet.setText("<html>✅ Complete jeepney route database<br><br>" +
+                "✅ Step-by-step travel instructions.<br><br>" +
+                "✅ Easy-to-use interface for all commuters</html>");
+        bullet.setFont(loadCustomFont("ProjectFiles/DMSans.ttf", 16f));
 
-        JTextField currentLocation = new JTextField();
-        currentLocation.setPreferredSize(new Dimension(250, 40));
-        JTextField destination = new JTextField();
-        destination.setPreferredSize(new Dimension(250, 40));
+        JButton start = new JButton("Start Now!");
+        start.setFont(loadCustomFont("ProjectFiles/DMSans.ttf", 16f));
+        start.setBackground(new Color(0xd85259));
+        start.setForeground(new Color(0xe8ced6));
+        start.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mainPage landingFrame = null;
+                try {
+                    landingFrame = new mainPage();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                } catch (FontFormatException ex) {
+                    throw new RuntimeException(ex);
+                }
+                landingFrame.setVisible(true);
+            }
+        });
 
-        inputContainer.add(currentLocation);
-        inputContainer.add(Box.createVerticalStrut(20));
-        inputContainer.add(destination);
+        ImageIcon image = new ImageIcon("ProjectFiles/MockUp.png");
+        Image scaled = image.getImage().getScaledInstance(475, 475, Image.SCALE_SMOOTH);
+        ImageIcon Img = new ImageIcon(scaled);
+        JLabel mockUp = new JLabel(Img, SwingConstants.CENTER);
 
-        textContainer.add(wcContainer);
-        textContainer.add(inputContainer);
+        infoText.add(wcText);
+        infoText.add(Box.createVerticalStrut(15));
+        infoText.add(heading);
+        infoText.add(Box.createVerticalStrut(10));
+        infoText.add(bullet);
+        infoText.add(Box.createVerticalStrut(20));
+        infoText.add(start);
 
-        JLabel resultLabel = new JLabel("We found 4 routes!");
-        resultLabel.setFont(loadCustomFont("ProjectFiles/DMSansItalic.ttf", 20f));
-        resultLabel.setForeground(new Color(0xd85259));
-        resultLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        infoContainer.add(infoText);
+        infoContainer.add(mockUp);
 
-        JPanel routeContainer = new JPanel();
-        routeContainer.setLayout(new GridLayout(2, 2, 25, 25));
-        routeContainer.setPreferredSize(new Dimension(1200, 400));
-        routeContainer.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel howText = new JLabel("How to use?");
+        howText.setFont(loadCustomFont("ProjectFiles/DMSansBold.ttf", 18f));
+        howText.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        roundPanel route1 = new roundPanel(30);
-        route1.setPreferredSize(new Dimension(250, 100));
-        route1.setBackground(Color.GRAY);
+        JPanel instructContainer = new JPanel();
+        instructContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        instructContainer.setPreferredSize(new Dimension(1000, 350));
 
-        roundPanel route2 = new roundPanel(30);
-        route2.setPreferredSize(new Dimension(250, 100));
-        route2.setBackground(Color.GRAY);
+        roundPanel step1 = new roundPanel(30);
+        step1.setLayout(new BoxLayout(step1, BoxLayout.Y_AXIS));
+        step1.setPreferredSize(new Dimension(375,350));
+        step1.setBackground(new Color(0xb7caef));
+        step1.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        roundPanel route3 = new roundPanel(30);
-        route3.setPreferredSize(new Dimension(250, 100));
-        route3.setBackground(Color.GRAY);
+        JLabel heading1 = new JLabel("Step 1");
+        heading1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        heading1.setFont(loadCustomFont("ProjectFiles/DMSansBold.ttf", 18f));
 
-        roundPanel route4 = new roundPanel(30);
-        route4.setPreferredSize(new Dimension(250, 100));
-        route4.setBackground(Color.GRAY);
+        roundPanel panel1 = new roundPanel(30);
+        panel1.setPreferredSize(new Dimension(30, 20));
+        panel1.setMaximumSize(new Dimension(300, 50));
+        panel1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel label1 = new JLabel("Enter your current location.");
+        label1.setFont(loadCustomFont("ProjectFiles/DMSans.ttf", 16f));
 
-        routeContainer.add(route1);
-        routeContainer.add(route2);
-        routeContainer.add(route3);
-        routeContainer.add(route4);
+        roundPanel panel11 = new roundPanel(30);
+        panel11.setPreferredSize(new Dimension(30, 20));
+        panel11.setMaximumSize(new Dimension(300, 50));
+        panel11.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel label11 = new JLabel("Enter your destination");
+        label11.setFont(loadCustomFont("ProjectFiles/DMSans.ttf", 16f));
 
+        JButton button1 = new JButton("Then press search!");
+        button1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button1.setPreferredSize(new Dimension(30, 20));
+        button1.setFont(loadCustomFont("ProjectFiles/DMSans.ttf", 16f));
+        button1.setBackground(new Color(0xd85259));
+        button1.setForeground(new Color(0xe8ced6));
 
-        contentPane.add(textContainer);
+        panel1.add(label1);
+        panel11.add(label11);
+
+        step1.add(Box.createVerticalStrut(30));
+        step1.add(heading1);
+        step1.add(Box.createVerticalStrut(20));
+        step1.add(panel1);
+        step1.add(Box.createVerticalStrut(15));
+        step1.add(panel11);
+        step1.add(Box.createVerticalStrut(20));
+        step1.add(button1);
+
+        roundPanel step2 = new roundPanel(30);
+        step2.setLayout(new BoxLayout(step2, BoxLayout.Y_AXIS));
+        step2.setPreferredSize(new Dimension(375,350));
+        step2.setBackground(new Color(0xffe786));
+        step2.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel heading2 = new JLabel("Step 2");
+        heading2.setAlignmentX(Component.CENTER_ALIGNMENT);
+        heading2.setFont(loadCustomFont("ProjectFiles/DMSansBold.ttf", 18f));
+
+        roundPanel panel2 = new roundPanel(30);
+        panel2.setPreferredSize(new Dimension(30, 20));
+        panel2.setMaximumSize(new Dimension(300, 50));
+        panel2.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel label2 = new JLabel("See the routes pop up!");
+        label2.setFont(loadCustomFont("ProjectFiles/DMSans.ttf", 16f));
+
+        ImageIcon routeimg = new ImageIcon("ProjectFiles/RouteImg.png");
+        Image sized = routeimg.getImage().getScaledInstance(400, 400, Image.SCALE_SMOOTH);
+        ImageIcon rimg = new ImageIcon(sized);
+        JLabel rImg = new JLabel(rimg, SwingConstants.CENTER);
+        rImg.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        panel2.add(label2);
+        step2.add(Box.createVerticalStrut(30));
+        step2.add(heading2);
+        step2.add(Box.createVerticalStrut(20));
+        step2.add(panel2);
+        step2.add(rImg);
+
+        roundPanel step3 = new roundPanel(30);
+        step3.setLayout(new BoxLayout(step3, BoxLayout.Y_AXIS));
+        step3.setPreferredSize(new Dimension(375,350));
+        step3.setBackground(new Color(0xb7caef));
+        step1.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel heading3 = new JLabel("Step 3");
+        heading3.setAlignmentX(Component.CENTER_ALIGNMENT);
+        heading3.setFont(loadCustomFont("ProjectFiles/DMSansBold.ttf", 18f));
+
+        roundPanel panel33 = new roundPanel(30);
+        panel33.setPreferredSize(new Dimension(30, 20));
+        panel33.setMaximumSize(new Dimension(300, 75));
+        panel33.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel label33 = new JLabel();
+        label33.setText("<html>Click a specific route for a<br>detailed overview of the<br>stops, transfers and more.</html>");
+        label33.setFont(loadCustomFont("ProjectFiles/DMSans.ttf", 16f));
+
+        panel33.add(label33);
+
+        step3.add(Box.createVerticalStrut(30));
+        step3.add(heading3);
+        step3.add(Box.createVerticalStrut(20));
+        step3.add(panel33);
+
+        instructContainer.add(step1);
+        instructContainer.add(step2);
+        instructContainer.add(step3);
+
+        contentPane.add(infoContainer);
         contentPane.add(Box.createVerticalStrut(30));
-        contentPane.add(resultLabel);
+        contentPane.add(howText);
         contentPane.add(Box.createVerticalStrut(30));
-        contentPane.add(routeContainer);
+        contentPane.add(instructContainer);
+
         center.add(contentPane);
-        mainPage.add(center, BorderLayout.CENTER);
-        mainPage.add(head, BorderLayout.NORTH);
-        add(mainPage, BorderLayout.CENTER);
+        add(head, BorderLayout.NORTH);
+        add(center, BorderLayout.CENTER);
 
         setVisible(true);
     }
@@ -124,5 +220,6 @@ public class landingPage extends JFrame {
         ge.registerFont(font);
         return font.deriveFont(size);
     }
+
 
 }
---
+++ src/main/java/org/example/mainPage.java
@@ -0,0 +1,129 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
+//green#84b477, red#d85259, #e8ced6, blue#b7caef, yellow#ffe786
+
+public class mainPage extends JFrame {
+    public mainPage() throws IOException, FontFormatException {
+        setSize(1920, 1080);
+        setTitle("Para!");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel mainPage = new JPanel();
+        mainPage.setPreferredSize(new Dimension(1644, 960));
+        mainPage.setLayout(new BorderLayout());
+
+        JPanel head = new JPanel();
+        head.setPreferredSize(new Dimension(1181, 160));
+        head.setOpaque(false);
+
+        ImageIcon img = new ImageIcon("ProjectFiles/Para.png");
+        Image resized = img.getImage().getScaledInstance(175, 175, Image.SCALE_SMOOTH);
+        ImageIcon logo = new ImageIcon(resized);
+        JLabel logoLabel = new JLabel(logo, SwingConstants.CENTER);
+
+        head.add(logoLabel, BorderLayout.CENTER);
+
+        JPanel center = new JPanel();
+
+        JPanel contentPane = new JPanel();
+        contentPane.setPreferredSize(new Dimension(1000, 800));
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+
+        roundPanel textContainer = new roundPanel(30);
+        textContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 150, 50));
+        textContainer.setBackground(new Color(0x84b477));
+        textContainer.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel wcContainer = new JPanel();
+        wcContainer.setLayout(new BoxLayout(wcContainer, BoxLayout.Y_AXIS));
+        wcContainer.setAlignmentX(Component.CENTER_ALIGNMENT);
+        wcContainer.setBackground(new Color(0x84b477));
+
+        JLabel wcText = new JLabel("Welcome Ken!");
+        wcText.setFont(loadCustomFont("ProjectFiles/DMSansItalic.ttf", 20f));
+        wcText.setForeground(new Color(0xffe786));
+        JLabel wcQuestion = new JLabel();
+        wcQuestion.setText("<html>Where do you want<br>to go?</html>");
+        wcQuestion.setFont(loadCustomFont("ProjectFiles/DMSansBold.ttf", 40f));
+
+        wcContainer.add(wcText);
+        wcContainer.add(Box.createVerticalStrut(20));
+        wcContainer.add(wcQuestion);
+
+        JPanel inputContainer = new JPanel();
+        inputContainer.setLayout(new BoxLayout(inputContainer, BoxLayout.Y_AXIS));
+        inputContainer.setAlignmentX(Component.CENTER_ALIGNMENT);
+        inputContainer.setBackground(new Color(0x84b477));
+
+        JTextField currentLocation = new JTextField();
+        currentLocation.setPreferredSize(new Dimension(250, 40));
+        JTextField destination = new JTextField();
+        destination.setPreferredSize(new Dimension(250, 40));
+
+        inputContainer.add(currentLocation);
+        inputContainer.add(Box.createVerticalStrut(20));
+        inputContainer.add(destination);
+
+        textContainer.add(wcContainer);
+        textContainer.add(inputContainer);
+
+        JLabel resultLabel = new JLabel("We found 4 routes!");
+        resultLabel.setFont(loadCustomFont("ProjectFiles/DMSansItalic.ttf", 20f));
+        resultLabel.setForeground(new Color(0xd85259));
+        resultLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel routeContainer = new JPanel();
+        routeContainer.setLayout(new GridLayout(2, 2, 25, 25));
+        routeContainer.setPreferredSize(new Dimension(1200, 400));
+        routeContainer.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        roundPanel route1 = new roundPanel(30);
+        route1.setPreferredSize(new Dimension(250, 100));
+        route1.setBackground(Color.GRAY);
+
+        roundPanel route2 = new roundPanel(30);
+        route2.setPreferredSize(new Dimension(250, 100));
+        route2.setBackground(Color.GRAY);
+
+        roundPanel route3 = new roundPanel(30);
+        route3.setPreferredSize(new Dimension(250, 100));
+        route3.setBackground(Color.GRAY);
+
+        roundPanel route4 = new roundPanel(30);
+        route4.setPreferredSize(new Dimension(250, 100));
+        route4.setBackground(Color.GRAY);
+
+        routeContainer.add(route1);
+        routeContainer.add(route2);
+        routeContainer.add(route3);
+        routeContainer.add(route4);
+
+
+        contentPane.add(textContainer);
+        contentPane.add(Box.createVerticalStrut(30));
+        contentPane.add(resultLabel);
+        contentPane.add(Box.createVerticalStrut(30));
+        contentPane.add(routeContainer);
+
+        center.add(contentPane);
+        mainPage.add(center, BorderLayout.CENTER);
+        mainPage.add(head, BorderLayout.NORTH);
+        add(mainPage, BorderLayout.CENTER);
+
+        setVisible(true);
+    }
+
+    private static Font loadCustomFont(String fontPath, float size) throws IOException, FontFormatException {
+        Font font = Font.createFont(Font.TRUETYPE_FONT, new File(fontPath));
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(font);
+        return font.deriveFont(size);
+    }
+
+}
